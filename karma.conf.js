f3cebce0eb6d015cd52a698ee0d10aa1575ad8d6
Leland Richardson <leland.m.richardson@gmail.com>
2016-01-30 11:53:40 -0800

    Initial support

---
+++ karma.conf.js
@@ -0,0 +1,61 @@
+module.exports = function(config) {
+  config.set({
+    basePath: '',
+    frameworks: ['mocha'],
+    files: [
+      'test/**/*.js'
+    ],
+
+    preprocessors: {
+      // add webpack as preprocessor
+      'src/**/*.js': ['webpack', 'sourcemap'],
+      'test/**/*.js': ['webpack', 'sourcemap']
+    },
+
+    webpack: { //kind of a copy of your webpack config
+      devtool: 'inline-source-map', //just do inline source maps instead of the default
+      module: {
+        loaders: [
+          {
+            test: /\.js$/,
+            exclude: /\/node_modules\//,
+            loader: 'babel',
+            query: {
+              presets: ['airbnb']
+            }
+          }
+        ]
+      },
+      externals: {
+        'cheerio': 'window',
+        'react/lib/ExecutionEnvironment': true,
+        'react/lib/ReactContext': true
+      }
+    },
+
+    webpackServer: {
+      noInfo: true //please don't spam the console when running in karma!
+    },
+
+    plugins: [
+      'karma-webpack',
+      'karma-mocha',
+      'karma-sourcemap-loader',
+      'karma-chrome-launcher'
+    ],
+
+
+    babelPreprocessor: {
+      options: {
+        presets: ['airbnb']
+      }
+    },
+    reporters: ['progress'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ['Chrome'],
+    singleRun: false,
+  })
+};
